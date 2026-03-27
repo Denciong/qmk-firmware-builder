a5d488952a03a354e37c007e9b1785784b50102f
Denciong <85654655+Denciong@users.noreply.github.com>
2026-03-27 14:41:34 -0400

    Update config.h

--- keyboards/nullbitsco/nibble/keymaps/custom-vial/config.h
+++ keyboards/nullbitsco/nibble/keymaps/custom-vial/config.h
@@ -1,24 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
-
 #pragma once
-
 #define VIAL_KEYBOARD_UID {0x2B, 0xBC, 0xB7, 0x62, 0x3F, 0x97, 0x84, 0x6E}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
 #define VIAL_UNLOCK_COMBO_COLS { 1, 6 }
-
 /* Add timeout for RGB lights */
 #define RGBLIGHT_SLEEP 
 #define RGBLIGHT_TIMEOUT 60000
-
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
-#define NO_ACTION_ONESHOT
-#define NO_ACTION_TAPPING
-
-/* Reduce number of layers */
-
+/* Layers */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
-
-// Disable SIO driver not needed for RP2040 conversion
+/* Disable SIO driver - not needed for RP2040 conversion */
 #undef HAL_USE_SIO
 #define HAL_USE_SIO FALSE
